db5420a681b201edc0785c70565c704f4e68f1be
Chris Philbin <chris@chrisphilbin.net>
2019-01-08 00:49:36 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,16 +1,9 @@
-require 'song.rb'
+
 
 class Artist
 
   attr_accessor :name
 
-  def initialize(name)
-    @name = name
-  end
 
 end
 
-lp = Artist.new("Linkin Park")
-new_divide = Song.new("New Divide")
-
-new_divide.artist = lp
